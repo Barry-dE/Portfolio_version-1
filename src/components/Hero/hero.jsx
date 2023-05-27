e08624ead4b3e6fbc07a14ef5f4157b8e55f6b86
Barry-dE <barigbuenbira@gmail.com>
2023-05-27 10:38:04 +0100

    HIM

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -1,3 +1,4 @@
+import image from "../../assets/img/diogo-nunes-Wa9ilX9XYOI-unsplash-300.jpg";
 import "./hero.scss";
 
 function Hero() {
@@ -8,17 +9,23 @@ function Hero() {
           <h1 className="hero-h1__text">
             I transform <span className="image"></span>
             <span className="hero-h1__text--italic">ideas</span> <br /> into
-            digital reality, <br /> one elegant solution <br />
-            at a time.
+            digital reality, <br /> one elegant s
+            <span className="round-border">
+              <img className="round-border__image" src={image} alt="" />
+            </span>
+            lution <br />
+            <span className=".round-border"></span>at a time.
           </h1>
         </div>
         <div className="occupation">
-          <span className="occupation__code">Software Developer</span>
-          <span></span>
+          <span className="occupation__code">Software Engineer</span>
+          <span className="occupation__dash"></span>
           <span className="occupation__write">Technical Writer</span>
         </div>
         <div className="Rotating-text">
-          <p>Rotating text fiels goes here</p>
+          <figure>
+            <img src={image} alt="" />
+          </figure>
         </div>
       </div>
     </div>
